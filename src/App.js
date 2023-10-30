98523da8fa44adc1cc0341d6ffba066c0bf8ad76
Saffet01 <saffetr.kaban@gmail.com>
2023-10-30 16:36:10 +0300

    TaskCreate values sent as props to App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList';
 
 function App() {
+
+  const createTask = (title, taskDesc) => {
+    console.log("Title: ", title);
+    console.log("Task Description: ", taskDesc);
+  };
+
   return (
     <div className="App">
-      <TaskCreate/>
+      <TaskCreate OnCreateProps={createTask}/>
       <h2>Tasks</h2>
       <TaskList/>
     </div>
--- src/components/TaskCreate.jsx
+++ src/components/TaskCreate.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function TaskCreate() {
+
+//OnCreateProps burada yarattığımız title ve description textlerini props olarak, App.js sayfasına göndermek amacı taşıyor.
+function TaskCreate({OnCreateProps}) {
+  const [title, setTitle] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+    console.log("title: ", title);
+  };
+
+  const [taskDesc, setTaskDesc] = useState("");
+
+  const handleTaskDesc = (event) => {
+    setTaskDesc(event.target.value);
+    console.log("Task Description: ", taskDesc);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    OnCreateProps(title, taskDesc);
+    setTitle("");
+    setTaskDesc("");
+  }
+
   return (
-    <div className='form-layout'>
+    <div className="form-layout">
       <h2>Define your task in below</h2>
-      <form className='task-form' action="">
-        <label htmlFor="">Task Title</label>
-        <input className='title-label' type="text" />
+      <form className="task-form" action="" onSubmit={handleSubmit}>
+        <label>Task Title</label>
+        <input
+          value={title}
+          onChange={handleTitleChange}
+          className="title-label"
+          type="text"
+        />
         <label htmlFor="">Task Information</label>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <button>Create</button>
+        <textarea
+          value={taskDesc}
+          onChange={handleTaskDesc}
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button onClick={handleSubmit}>Create</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default TaskCreate
+export default TaskCreate;
